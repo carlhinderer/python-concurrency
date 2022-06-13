ac070937a8d5bed1d1030a81ad24c90913aab7f0
Carl Hinderer <carl.hinderer4@gmail.com>
2022-06-13 12:24:57 -0600

    Added example of using 'join' to control execution flow.

---
+++ learning_concurrency_in_python/code/ch04/thread_join.py
@@ -0,0 +1,24 @@
+import threading
+import time
+
+
+def our_thread(i):
+    print("Thread {} Started".format(i))
+    time.sleep(i*2)
+    print("Thread {} Finished".format(i))
+
+def main():
+    thread = threading.Thread(target=our_thread, args=(1,))
+    thread.start()
+
+    print("Is thread 1 Finished?")
+
+    thread2 = threading.Thread(target=our_thread, args=(2,))
+    thread2.start()
+    thread2.join()
+
+    print("Thread 2 definitely finished")
+
+
+if __name__ == '__main__':
+    main()
